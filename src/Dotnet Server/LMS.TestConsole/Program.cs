88bdd175713a8a89c4e3b06d8921a3e9de4affed
Neville Nazerane <neville@mediroutes.com>
2025-12-26 01:20:36 -0700

    fixed http calls

--- src/Dotnet Server/LMS.TestConsole/Program.cs
+++ src/Dotnet Server/LMS.TestConsole/Program.cs
@@ -6,11 +6,10 @@ using System.Net.Http.Json;
 
 var client = new HttpClient
 {
-    BaseAddress = new("http://localhost:5100")
+    BaseAddress = new("https://localhost:7283")
 };
 
 
 var teachers = await client.GetFromJsonAsync<IEnumerable<Teacher>>("teachers");
-
 
 teachers = await client.GetFromJsonAsync<IEnumerable<Teacher>>("teachers");
