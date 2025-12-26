a0025ae4a3f35521481b1145117095c2daccf499
Neville Nazerane <neville@mediroutes.com>
2025-12-25 20:42:45 -0700

    sb setup

--- src/Dotnet Server/LMS.Models/StudyProgramCourse.cs
+++ src/Dotnet Server/LMS.Models/StudyProgramCourse.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace LMS.Models
 {
+
+    [PrimaryKey(nameof(StudyProgramId), nameof(CourseId))]
     public class StudyProgramCourse
     {
-        public int Id { get; set; }
-
         [Required]
         public int? StudyProgramId { get; set; }
 
---
+++ src/Dotnet Server/LMS.WebAPI/Services/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using LMS.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LMS.WebAPI.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Teacher> Teachers => Set<Teacher>();
+
+        public DbSet<Student> Students => Set<Student>();
+
+        public DbSet<StudyProgram> StudyPrograms => Set<StudyProgram>();
+
+        public DbSet<Course> Courses => Set<Course>();
+
+        public DbSet<CourseSection> CourseSections => Set<CourseSection>();
+
+        public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+
+        public DbSet<StudyProgramCourse> StudyProgramCourses => Set<StudyProgramCourse>();
+    }
+
+}
