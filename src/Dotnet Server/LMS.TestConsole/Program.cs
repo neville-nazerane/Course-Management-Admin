87b35595ad6984808f56134c8f5bbea4ea005938
Neville Nazerane <neville@mediroutes.com>
2025-12-26 00:02:36 -0700

    testing console app

---
+++ src/Dotnet Server/LMS.TestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using LMS.Models;
+using System.Net.Http.Json;
+
+
+
+
+var client = new HttpClient
+{
+    BaseAddress = new("http://localhost:5100")
+};
+
+
+var teachers = await client.GetFromJsonAsync<IEnumerable<Teacher>>("teachers");
+
+
+teachers = await client.GetFromJsonAsync<IEnumerable<Teacher>>("teachers");
